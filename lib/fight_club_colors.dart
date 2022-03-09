d986e0a9e39442042357bc96e5a0edafca3ae039
Soundtrack10 <armdo@yandex.ru>
2022-03-09 22:18:46 +0300

    New Commit

---
+++ lib/fight_club_colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FightClubColors {
+
+  FightClubColors._();
+
+
+  static const background = const Color(0xFFD5DEF0);
+  static const greyButton = Colors.black38;
+  static const blueButton = const Color.fromRGBO(28, 121, 206, 1);
+  static const blackButton = const Color.fromRGBO(0,0,0, 0.87);
+  static const darkGreyText = Color(0xFF161616);
+  static const whiteText = Color.fromRGBO(255, 255, 255, 0.87);
+  static const darkPurple = Color(0xFFC5D1EA);
+}
